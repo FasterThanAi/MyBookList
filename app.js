73a6e74add2aab4ed83cd1ce4629b9520f5a0758
FasterThanAi <demeanor404@gmail.com>
2026-01-03 00:03:38 +0530

    Fix query selectors for book form inputs and prevent default form submission

--- app.js
+++ app.js
@@ -43,9 +43,11 @@ document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 document.getElementById('book-form').addEventListener('submit',(e)=>{
     // get form value
-    const title=document.querySelector('title').value
-    const author=document.querySelector('author').value
-    const isbn=document.querySelector('isbn').value
+    // prevent dafault action
+    e.preventDefault();
+    const title=document.querySelector('#title').value
+    const author=document.querySelector('#author').value
+    const isbn=document.querySelector('#isbn').value
     // intialise book 
     const book=new Book(title,author,isbn);
 

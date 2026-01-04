ea9e9a07abf5faaf283cbbc2c2ef2c4ab9d7ef61
FasterThanAi <demeanor404@gmail.com>
2026-01-05 00:00:52 +0530

    Add alert functionality for book addition and deletion, and validate form inputs

--- app.js
+++ app.js
@@ -38,6 +38,27 @@ class UI{
                 ele.parentElement.parentElement.remove();
             }
         }
+        // 
+        // <div class="alert alert-danger"></div>
+        static showAlert(message , className){
+            const div=document.createElement('div')
+            if(className === 'danger') {
+            div.className = `bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4`;
+        } else {
+            div.className = `bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4`;
+        }
+            div.appendChild(document.createTextNode(message));
+            // placeholder to display
+            const container=document.querySelector('.container') 
+            const form=document.querySelector('#book-form');
+            // We want to put the alert INSIDE the white card, just above the form
+        // The parent of the form is the white card div
+            form.parentElement.insertBefore(div, form);
+            // 3 sec alert vanish 
+            setTimeout(() => document.querySelector('.bg-red-100, .bg-green-100').remove()
+                
+            , 3000);
+        }
         static clearFields(){
             document.querySelector('#title').value=''
             document.querySelector('#author').value=''
@@ -61,15 +82,27 @@ document.getElementById('book-form').addEventListener('submit',(e)=>{
     const title=document.querySelector('#title').value
     const author=document.querySelector('#author').value
     const isbn=document.querySelector('#isbn').value
-    // intialise book 
-    const book=new Book(title,author,isbn);
+
+    // validate 
+    if(title==='' || author===''|| isbn===''){
+        UI.showAlert('Please Fill all the column ','danger')
+    }
+    else{
+        const book=new Book(title,author,isbn);
 
     // console.log(book);
     // add book to list
 
     UI.addBookToList(book)
+
+    // show book success fully added validation 
+    UI.showAlert('Book Added Successfully','success')
     // clear field 
+
     UI.clearFields();
+    }
+    // intialise book 
+    
 })
 
 
@@ -79,5 +112,7 @@ document.querySelector('#book-list').addEventListener('click',(e)=>{
     // console.log(e.target);
     UI.deleteBook(e.target)
     
+    //show message of book removed 
+    UI.showAlert('Book Deleted Successfully','success')
 })
 
